6e0e505fcc840479a2772d17e0840b3902045748
Tejastatestvagrant <tejass@testvagrant.com>
2024-07-18 17:55:39 +0530

    Added locators and page actions for ProfilePage ,registration Page , Login page

---
+++ ui/screens/common/ProfileScreens/forgotPasswordScreenPage.ts
@@ -0,0 +1,58 @@
+import { Element } from 'webdriverio';
+import { BaseScreen } from '../../../../uiExport';
+
+export class ForgotPasswordScreenPage extends BaseScreen {
+  private selectors = {
+    emailInput: { android: "//*[@text='Enter your email']", ios: '~inp-email' },
+    submitButton: { android: "//*[@text='Submit']", ios: '~icon-next' },
+    forgotPasswordHeader: { ios: '~txt-forgot-password' },
+    emptyEmailField:{ios: '~txt-email-field-cannot-be-empty'},
+    newPasswordInput: { android: "//*[@text='Enter your new password']", ios: '~inp-new-password' },
+    confirmPasswordInput: { android: "//*[@text='Confirm password']", ios: '~inp-confirm-password' },
+    resetPasswordButton: { android: "//*[@text='Reset password']", ios: '~btn-reset-password' },
+    resetpasswordSuccessfull: { ios: "//*[@name='Your password has been reset Successfully'])[5]" }
+  };
+
+  async emailInputEle(): Promise<Element<'async'>> {
+    return this.getElement(this.selectors.emailInput.ios);
+  }
+
+  async submitButtonEle(): Promise<Element<'async'>> {
+    return this.getElement(this.selectors.submitButton.ios);
+  }
+
+  async backButtonEle(): Promise<Element<'async'>> {
+    return this.getElement(this.selectors.forgotPasswordHeader.ios);
+  }
+
+  async emptyEmailEle(): Promise<Element<'async'>> {
+    return this.getElement(this.selectors.emptyEmailField.ios);
+  }
+
+  async fillForgotPasswordEmail(email: string) {
+    await this.setValue(await this.emailInputEle(), email);
+    await this.click(await this.submitButtonEle());
+  }
+
+  async newPasswordInputEle(): Promise<Element<'async'>> {
+    return this.getElement(this.selectors.newPasswordInput.ios);
+  }
+
+  async confirmPasswordInputEle(): Promise<Element<'async'>> {
+    return this.getElement(this.selectors.confirmPasswordInput.ios);
+  }
+
+  async resetPasswordButtonEle(): Promise<Element<'async'>> {
+    return this.getElement(this.selectors.resetPasswordButton.ios);
+  }
+
+  async fillResetPasswordDetails(passwordDetails: { newPassword: string; confirmPassword: string }) {
+    await this.setValue(await this.newPasswordInputEle(), passwordDetails.newPassword);
+    await this.setValue(await this.confirmPasswordInputEle(), passwordDetails.confirmPassword);
+    await this.click(await this.resetPasswordButtonEle());
+  }
+
+  async tapBackButton() {
+    await this.click(await this.backButtonEle());
+  }
+}
--- ui/screens/common/ProfileScreens/loginScreenPage.ts
+++ ui/screens/common/ProfileScreens/loginScreenPage.ts
@@ -1,37 +1,47 @@
 import { Element } from 'webdriverio';
 import { BaseScreen, XpathUtil } from '../../../../uiExport';
 
-export class LoginScreen extends BaseScreen {
+export class LoginScreenPage extends BaseScreen {
   private selectors = {
-    usernameText: { android: "//*[@text='Email']", ios: "//*[@name='test-Username']" },
-    passwordText: { android: "//*[@text='Password']", ios: "//*[@name='test-Password']" },
-    loginButton: { android: "//*[@text='Login']", ios: "//*[@name='test-LOGIN']" },
-    forgotPasswordLink: { android: "//*[@text='Forgot Password?']", ios: 'TODO: Implement iOS locator for Forgot Password Link' },
+    usernameInput: { android: "//*[@text='Email']", ios: '~inp-email' },
+    passwordInput: { android: "//*[@text='Password']", ios: '~inp-password' },
+    loginButton: { android: "//*[@text='Login']", ios: '~btn-login' },
+    forgotPasswordLink: { android: "//*[@text='Forgot Password?']", ios: '~txt-forgot-password' },
+    registerLink:{ios:'~btn-register'},
+    backButton :{ios:'~btn-back'},
+    usernameFormateError :{ios:'~txt-email-format-is-incorrect'}
   };
 
-  async usernameTextEle(): Promise<Element<'async'>> {
-    return this.getElement(XpathUtil.getXpath(this.driver, this.selectors.usernameText));
+  async usernameInputEle(): Promise<Element<'async'>> {
+    return this.getElement(this.selectors.usernameInput.ios);
   }
 
-  async passwordTextEle(): Promise<Element<'async'>> {
-    return this.getElement(XpathUtil.getXpath(this.driver, this.selectors.passwordText));
+  async passwordInputEle(): Promise<Element<'async'>> {
+    return this.getElement(this.selectors.passwordInput.ios);
   }
 
   async loginButtonEle(): Promise<Element<'async'>> {
-    return this.getElement(XpathUtil.getXpath(this.driver, this.selectors.loginButton));
+    return this.getElement(this.selectors.loginButton.ios);
   }
 
   async forgotPasswordLinkEle(): Promise<Element<'async'>> {
-    return this.getElement(XpathUtil.getXpath(this.driver, this.selectors.forgotPasswordLink));
+    return this.getElement(this.selectors.forgotPasswordLink.ios);
   }
-
+  
+  async registerLinkEle(): Promise<Element<'async'>> {
+    return this.getElement(this.selectors.forgotPasswordLink.ios);
+  }
   async fillLoginDetails(accountDetails: { username: string; password: string }) {
-    await this.setValue(await this.usernameTextEle(), accountDetails.username);
-    await this.setValue(await this.passwordTextEle(), accountDetails.password);
+    await this.setValue(await this.usernameInputEle(), accountDetails.username);
+    await this.setValue(await this.passwordInputEle(), accountDetails.password);
     await this.click(await this.loginButtonEle());
   }
 
   async tapForgotPasswordLink() {
     await this.click(await this.forgotPasswordLinkEle());
+  }
+
+  async tapOnRegisterLink(){
+      await this.click(await this.registerLinkEle());
   }
 }
