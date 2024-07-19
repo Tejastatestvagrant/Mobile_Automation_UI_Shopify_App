6cbc0ead3c6fe38326145e440b1374691dc07999
vineetb0901 <vineetb@testvagrant.com>
2024-07-19 17:03:06 +0530

    added product screen locators

---
+++ ui/screens/common/ProductScreens/productScreen.ts
@@ -0,0 +1,30 @@
+import { Element } from 'webdriverio';
+import { BaseScreen, XpathUtil } from '../../../../uiExport';
+
+export class ProductScreen extends BaseScreen {
+    private selectors = {
+        headerLogo: { android: "//*[@content-desc='appHeader']", ios: "//*[@name='assets/assets/icons/ultralesson-logo.png']" },
+        searchInputBtn: { android: "//*[@content-desc='searchBar']", ios: "//*[@name='btn-search']" },
+        backButton: { android: "", ios: "~btn-back" },
+        productName: { android: "", ios: "~txt-product-name" },
+        productPrice: { android: "", ios: "~txt-product-price" },
+        productDescription: { android: "", ios: "~txt-product-description" },
+        featureDropDown: { android: "", ios: "~txt-features" },
+        addToCartButton: { android: "", ios: `-ios predicate string:name == "btn-cart" AND label == "Add To Cart"` },
+        productRatingWithCategory: { android: "", ios: "~txt-rating-category" },
+        goToCartButton: { android: "", ios: `-ios predicate string:name == ""btn-cart" AND label == "Go To Cart"` },
+    };
+
+    async addToCartButton(): Promise<Element<'async'>> {
+        return this.getElement(this.selectors.addToCartButton.ios);
+    }
+
+    async productDescription(): Promise<Element<'async'>> {
+        return this.getElement(this.selectors.addToCartButton.ios);
+    }
+    async ratingWithCategory(): Promise<Element<'async'>> {
+        return this.getElement(this.selectors.productRatingWithCategory.ios);
+    }
+
+
+}
