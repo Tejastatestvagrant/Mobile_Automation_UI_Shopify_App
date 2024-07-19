6ccc29a3d21774d3f67998cafe9ab3e8e33e6e19
vineetb0901 <vineetb@testvagrant.com>
2024-07-19 23:08:06 +0530

    added explore screen files and tests

--- ui/screens/common/ExploreScreens/exploreScreenPage.ts
+++ ui/screens/common/ExploreScreens/exploreScreenPage.ts
@@ -1,12 +1,17 @@
 import { Element } from 'webdriverio';
-import { BaseScreen, XpathUtil } from '../../../uiExport';
+import { BaseScreen, XpathUtil } from '../../../../uiExport';
 
 export class ExploreScreen extends BaseScreen {
   private selectors = {
     backButton: { android: "//*[@content-desc='backButton']", ios: "//*[@name='backButton']" },
-    searchInput: { android: "//*[@content-desc='searchBar']", ios: "//*[@name='searchBar']" },
-    searchIcon: { android: "//*[@content-desc='searchIcon']", ios: "//*[@name='searchIcon']" },
-    exploreTitle: { android: "//android.widget.TextView[@text='Explore']", ios: "//XCUIElementTypeStaticText[@name='Explore']" },
+    searchInput: { android: "//*[@content-desc='searchBar']", ios: "~inp-search" },
+    searchIcon: { android: "//*[@content-desc='searchIcon']", ios: '//XCUIElementTypeOther[@name="icon-search"]/XCUIElementTypeOther/XCUIElementTypeOther' },
+    exploreTitle: { android: "//android.widget.TextView[@text='Explore']", ios: '//XCUIElementTypeTextField[@name="inp-search"]' },
+    errorPopup: { android: '', ios: '(//XCUIElementTypeOther[@name="Uh oh, something went wrong Search field cannot be empty"])[4]'},
+    noProductFoundMessage: { android: '', ios: '~txt-no-products-found'},
+    moreButton: { android: '', ios:'~txt-more'},
+    suggestionListElement: { android:'', ios: "//*[@name='ele-auto-suggestion']"},
+    suggestedProductName: { android: '', ios: '~txt-product-name'}
   };
 
   async backButtonEle(): Promise<Element<'async'>> {
@@ -65,4 +70,36 @@ export class ExploreScreen extends BaseScreen {
     const title = await this.getExploreTitle();
     return isScreenDisplayed && title === 'Explore';
   }
+
+  async isErrorMessageDisplayed(): Promise<boolean> {
+    const errorMessage = await this.getElement(XpathUtil.getXpath(this.driver,this.selectors.errorPopup))
+    return this.isDisplayed(errorMessage);
+  }
+  
+
+  async isNoProductMessageDisplayed(): Promise<boolean> {
+    const message = await this.getElement(this.selectors.noProductFoundMessage.ios)
+    return this.isDisplayed(message);
+  }
+
+  async isMoreButtonDisplayed(): Promise<boolean> {
+    const moreButton = await this.getElement(this.selectors.moreButton.ios)
+    return this.isDisplayed(moreButton);
+  }
+  
+  async tapOnMoreButton() {
+    const moreButton = await this.getElement(this.selectors.moreButton.ios);
+    await this.click(moreButton);
+  }
+
+  async getSuggestionListElementCount(): Promise<number> {
+    const elementCount = await this.getElements(this.selectors.suggestionListElement.ios);
+    return elementCount.length
+  }
+
+  async getSuggestedProductName(): Promise<string> {
+    const productName = await this.getElement(this.selectors.suggestedProductName.ios);
+    return this.getText(productName);
+  }
+
 }
---
+++ ui/specs/sanity/app/exploreScreen.test.ts
@@ -0,0 +1,61 @@
+/**
+ * @group Sanity
+ */
+
+import { Browser } from 'webdriverio';
+import { expect } from 'chai';
+import { Driver, ExploreScreen, HomeScreen } from '../../../../uiExport';
+import { HomeScreenUiValidationAction } from '../../../screens/userActions/HomeScreenActions/homeScreenUiValidationAction';
+
+let driver: Browser<'async'>;
+let homeScreen: HomeScreen;
+let exploreScreen: ExploreScreen;
+let homeScreenUiValidationAction: HomeScreenUiValidationAction;
+
+declare let reporter: any;
+const specName = 'Explore Screen test';
+describe(specName, () => {
+  beforeAll(async () => {
+    driver = await Driver.getDriver(specName);
+    exploreScreen = new ExploreScreen(driver);
+    homeScreen = new HomeScreen(driver);
+    homeScreenUiValidationAction = new HomeScreenUiValidationAction(driver);
+
+    await homeScreenUiValidationAction.tapExploreButton();
+  });
+
+  afterEach(async () => {
+    await Driver.attachScreenshots(driver, reporter);
+  });
+
+  afterAll(async () => {
+    await Driver.closeDrivers([driver]);
+  });
+
+  it('verify Verify Search with Empty Keyword', async () => {
+    expect(await exploreScreen.isExploreScreenDisplayed()).to.be.true;
+    await exploreScreen.enterSearchQuery('')
+    await exploreScreen.tapSearchIcon()
+    expect(await exploreScreen.isErrorMessageDisplayed()).to.be.true;
+    
+  });
+
+  it('Verify Search special characters', async () => {
+    await exploreScreen.enterSearchQuery('#')
+    await exploreScreen.tapSearchIcon()
+    expect(await exploreScreen.isNoProductMessageDisplayed()).to.be.true;
+  });
+
+  it('Verify more button in the suggestion list', async () => {
+    await exploreScreen.enterSearchQuery('hi')
+    expect(await exploreScreen.isMoreButtonDisplayed()).to.be.true
+    expect(await exploreScreen.getSuggestionListElementCount()).to.be.equal(3)
+    await exploreScreen.tapOnMoreButton()
+    expect(await exploreScreen.getSuggestionListElementCount()).to.greaterThan(3)
+  });
+
+  it('Verify product displayed with valid search query', async () => {
+    await exploreScreen.enterSearchQuery('High-Fidelity Headphones')
+    expect(await exploreScreen.getSuggestedProductName()).to.be.equal('High-Fidelity Headphones')
+  });
+});
